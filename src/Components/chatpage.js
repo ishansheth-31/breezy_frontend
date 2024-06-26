1c07fdbd08e1cb9a634e4377a03c7593ce4fd857
Ishan Sheth <kevishan@gmail.com>
2024-06-25 23:21:50 -0400

    Update chatpage.js

--- src/Components/chatpage.js
+++ src/Components/chatpage.js
@@ -86,6 +86,7 @@ const ChatPage = ({
             }
         } catch (err) {
             console.error('Error fetching TTS data:', err);
+            if (retryCount < maxRetries) {
                 console.log(`Retrying TTS fetch... Attempt ${retryCount + 1}`);
                 fetchAndPlayAudio(responseText, retryCount + 1);
             }
