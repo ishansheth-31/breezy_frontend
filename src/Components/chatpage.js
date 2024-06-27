bb7b933e27b2e4a5619c7350b05ff6a3fd901403
Ishan Sheth <kevishan@gmail.com>
2024-06-26 21:50:04 -0400

    Update chatpage.js

--- src/Components/chatpage.js
+++ src/Components/chatpage.js
@@ -45,7 +45,7 @@ const ChatPage = ({
         }
     };
 
-    const splitTextIntoChunks = (text, chunkSize = 500) => {
+    const splitTextIntoChunks = (text, chunkSize = 400) => {
         const chunks = [];
         for (let i = 0; i < text.length; i += chunkSize) {
             chunks.push(text.substring(i, i + chunkSize));
